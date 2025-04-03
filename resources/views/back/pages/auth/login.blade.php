52f13097644a482ee7af3b2da09aa11334033c90
rizkifadillah <rizkifadillah26.rf@gmail.com>
2025-04-03 16:48:49 +0700

    secure dashboard dan forget password

--- resources/views/back/pages/auth/login.blade.php
+++ resources/views/back/pages/auth/login.blade.php
@@ -58,7 +58,7 @@
             </div>
             <div class="col-6">
                 <div class="forgot-password">
-                    <a href="forgot-password.html">Forgot Password</a>
+                    <a href="{{ route('admin.forget') }}">Forgot Password</a>
                 </div>
             </div>
         </div>
